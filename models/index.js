7255617c23fb2183f3e7754fdf7e6458a74214d3
JustDesmarais <143149797+JustDesmarais@users.noreply.github.com>
2023-12-03 15:28:52 -0600

    build models and index file

--- models/index.js
+++ models/index.js
@@ -1,16 +1,31 @@
-// import models
+
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL',
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: 'product_tag',
+  foreignKey: 'id'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: 'product_tag',
+  foreignKey: 'id'
+})
 
 module.exports = {
   Product,
